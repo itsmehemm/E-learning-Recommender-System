f31d3b9a80fb4adba963eef01cdcfb24d6ad93c4
itsmehemm <hemanthprasathmurali@gmail.com>
2018-02-18 00:49:16 +0530

    Rake untouched test

---
+++ test2.py
@@ -0,0 +1,10 @@
+import rake
+import operator
+rake_object = rake.Rake("SmartStoplist.txt", 3, 3, 1)
+ 
+text = "Natural language processing (NLP) deals with the application of computational models to text or speech data. Application areas within NLP include automatic (machine) translation between languages; dialogue systems, which allow a human to interact with a machine using natural language; and information extraction, where the goal is to transform unstructured text into structured (database) representations that can be searched and browsed in flexible ways. NLP technologies are having a dramatic impact on the way people interact with computers, on the way people interact with each other through the use of language, and on the way people access the vast amount of linguistic data now in electronic form. From a scientific viewpoint, NLP involves fundamental questions of how to structure formal models (for example statistical models) of natural language phenomena, and of how to design algorithms that implement these models. In this course you will study mathematical and computational models of language, and the application of these models to key problems in natural language processing. The course has a focus on machine learning methods, which are widely used in modern NLP systems: we will cover formalisms such as hidden Markov models, probabilistic context-free grammars, log-linear models, and statistical models for machine translation. The curriculum closely follows a course currently taught by Professor Collins at Columbia University, and previously taught at MIT."
+ 
+keywords = rake_object.run(text)
+ 
+print "keywords: ", keywords
+
