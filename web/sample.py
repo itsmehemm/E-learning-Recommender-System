7f94f6b0037f30e07b1986c05dd4375b4aae9e9f
itsmehemm <hemanthprasathmurali@gmail.com>
2018-02-18 00:56:22 +0530

    compare topic vs token

---
+++ web/sample.py
@@ -0,0 +1,7 @@
+topic = ["database", "index", "machine", "learning"]
+token = ["machine", "learning", "cloud"]
+
+for i in range(0, len(topic)):
+	for j in range(0, len(token)):
+		if(topic[i] == token[j]):
+			print("success")
